e61a904d39f5d6324f9021dbbfb5c033bf4858c8
isaac <isw9@case.edu>
2018-09-15 01:19:16 -0400

    Working A Star Solver With Both Heuristics

--- AStarSolver.java
+++ AStarSolver.java
@@ -3,67 +3,72 @@ import java.util.*;
 
 public class AStarSolver {
   public static String[] directions = {"up", "down", "left", "right"};
-  public static PriorityQueue<PuzzleSolver> pQueue = new PriorityQueue<PuzzleSolver>(100, new BoardComparator());
-  public static Hashtable<String, int[][]> table = new Hashtable<String, int[][]>();
+  public static PriorityQueue<PuzzleSolver> pQueue = new PriorityQueue<PuzzleSolver>(10000000, new BoardComparator());
+  public static HashSet<int[][]> set = new HashSet<int[][]>();
   public static boolean goalFound = false;
-  
+
+  //
   public static void solve(String heuristic, PuzzleSolver node) {
     if (heuristic.equals("h1")) {
-      node.heuristic = h1(node);
+      node.heuristic = h1(node.board);
     }
     else if (heuristic.equals("h2")) {
-      node.heuristic = h2(node);
+      node.heuristic = h2(node.board);
     }
     else {
       System.out.println("Enter a valid heuristic");
     }
-    table.clear();
+    //clear the priority queue and hash table so we know they are empty
+    set.clear();
     pQueue.clear();
     goalFound = false;
-    
-    String key = hashKey(node.board);
-    table.put(key, node.board);
+    set.add(node.board);
     pQueue.add(node);
-    System.out.println(node);
-    //System.out.println(isGoalState(node.board));
-    int i = 0;
-    while (pQueue.size() != 0 && goalFound == false && i < 1000) {
+
+    //keep picking a node to expand from the priority queue until the puzzle is solved
+    while (pQueue.size() != 0 && goalFound == false) {
       PuzzleSolver currentNode = pQueue.poll();
       if (!isGoalState(currentNode.board)) {
-        expandNode(currentNode);
-        i++;
+        expandNode(currentNode, heuristic);
       }
       else {
         System.out.println("Heuristic: " + currentNode.heuristic);
+        System.out.println("Path: " + currentNode.path.substring(2));
         System.out.println("Path: " + currentNode.path);
         System.out.println("COMPLETE");
       }
     }
   }
   
-  public static void expandNode(PuzzleSolver node) {
+  //When expanding a node, only add its successor to the queue if it has not already been examined
+  public static void expandNode(PuzzleSolver node, String heuristic) {
     for (int i = 0; i < 4; i++) {
       int[][] currentBoard = node.board;
-      int[][] currentBoardCopy = currentBoard;
-      if (validMove(directions[i], currentBoardCopy)) {
-        int[][] boardPosition = starMove(directions[i], node.board);
-        System.out.println(alreadyExpanded(boardPosition));
+      if (validMove(directions[i], currentBoard)) {
+        int[][] boardPosition = starMove(directions[i], currentBoard);
         if (!alreadyExpanded(boardPosition)) { 
+          StringBuilder path = new StringBuilder();
+          path.append(node.path).append(",").append(directions[i].substring(0,1));
+          int heuris = 0;
+          if (heuristic.equals("h1")) {
+            heuris = h1(boardPosition);
+          }
+          else {
+            heuris = h2(boardPosition);
+          }
           PuzzleSolver unexploredNode = new PuzzleSolver(boardPosition, 
-                                                       node.heuristic, 
-                                                       //node.path + directions[i]);
-                                                         directions[i]);
+                                                         heuris, 
+                                                         path.toString());
           pQueue.add(unexploredNode);
-          String key = hashKey(boardPosition);
-          table.put(key, boardPosition);
+          set.add(boardPosition);
         }
       }
     }
   }
   
   public static boolean validMove(String direction, int[][] puzzle) {
-    int row = getBlankRow(puzzle);
-    int column = getBlankColumn(puzzle);
+    int row = getRow(puzzle, 0);
+    int column = getColumn(puzzle, 0);
     if (direction.equals("up")) {
       if (row == 1 || row == 2) {
         return true;
@@ -91,10 +96,10 @@ public class AStarSolver {
   }
   
   
-  public static int getBlankColumn(int[][] puzzle) {
+  public static int getColumn(int[][] puzzle, int target) {
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
-        if (puzzle[i][j] == 0) {
+        if (puzzle[i][j] == target) {
           return j;
         }
       }
@@ -102,10 +107,10 @@ public class AStarSolver {
     return -1;
   }
   
-  public static int getBlankRow(int[][] puzzle) {
+  public static int getRow(int[][] puzzle, int target) {
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
-        if (puzzle[i][j] == 0) {
+        if (puzzle[i][j] == target) {
           return i;
         }
       }
@@ -134,8 +139,8 @@ public class AStarSolver {
         result[i][j] = board[i][j];
       }
     }
-    int column = getBlankColumn(board);
-    int row = getBlankRow(board);
+    int column = getColumn(board, 0);
+    int row = getRow(board, 0);
     if (direction.equals("up")) {
       if (row == 1 || row == 2) {
         result[row][column] = board[row-1][column];
@@ -177,18 +182,30 @@ public class AStarSolver {
   }
   
   public static boolean alreadyExpanded(int[][] board) {
-    String key = hashKey(board);
-    return table.containsKey(key);
+    return set.contains(board);
   }
   
-  public static int h2(PuzzleSolver node) {
-    return 2;
+  public static int h2(int[][] board) {
+    int count = 0;
+    int expected = 0;
+
+    int[][] finalBoard = { {0, 1, 2}, {3, 4, 5}, {6, 7, 8} };
+    
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        int value = board[i][j];
+        expected++;
+        if (value != 0 && value != expected) {
+          count = count + Math.abs(i - getRow(finalBoard, value)) + Math.abs(j- getColumn(finalBoard, value));
+        }
+       }
+     }
+    return count;
   }
   
-  public static int h1(PuzzleSolver node) {
+  public static int h1(int[][] board) {
     int misplacedTiles = 0;
     int goalTile = 0;
-    int[][] board = node.board;
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
         if (board[i][j] != goalTile) {

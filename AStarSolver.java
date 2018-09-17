d5894265d1e3b934e88a93f65da90a5d94afd11a
isaac <isw9@case.edu>
2018-09-17 09:32:32 -0400

    Clean up code

--- AStarSolver.java
+++ AStarSolver.java
@@ -1,14 +1,16 @@
 import java.util.PriorityQueue;
 import java.util.*;
 
+// This class is called from the main Node class
+// AStarSolver is passed a Node and a heuristic and solves the 8 puzzle using A* search
 public class AStarSolver {
   public static String[] directions = {"up", "down", "left", "right"};
-  public static PriorityQueue<PuzzleSolver> pQueue = new PriorityQueue<PuzzleSolver>(10000000, new BoardComparator());
+  public static PriorityQueue<Node> pQueue = new PriorityQueue<Node>(10000000, new BoardComparator());
   public static HashSet<int[][]> set = new HashSet<int[][]>();
   public static boolean goalFound = false;
 
-  //
-  public static void solve(String heuristic, PuzzleSolver node) {
+  // The Node class enters here
+  public static void solve(String heuristic, Node node) {
     if (heuristic.equals("h1")) {
       node.heuristic = Util.h1(node.board);
     }
@@ -18,16 +20,16 @@ public class AStarSolver {
     else {
       System.out.println("Enter a valid heuristic");
     }
-    //clear the priority queue and hash table so we know they are empty
+    // clear the priority queue and hash table so we know they are empty and THEN add the first node
     set.clear();
     pQueue.clear();
     goalFound = false;
     set.add(node.board);
     pQueue.add(node);
 
-    //keep picking a node to expand from the priority queue until the puzzle is solved
+    // keep picking a node to expand from the priority queue until the puzzle is solved
     while (pQueue.size() != 0 && goalFound == false) {
-      PuzzleSolver currentNode = pQueue.poll();
+      Node currentNode = pQueue.poll();
       if (!isGoalState(currentNode.board)) {
         expandNode(currentNode, heuristic);
       }
@@ -39,7 +41,7 @@ public class AStarSolver {
   }
   
   //When expanding a node, only add its successor to the queue if it has not already been examined
-  public static void expandNode(PuzzleSolver node, String heuristic) {
+  public static void expandNode(Node node, String heuristic) {
     for (int i = 0; i < 4; i++) {
       int[][] currentBoard = node.board;
       if (Util.validMove(directions[i], currentBoard)) {
@@ -54,7 +56,7 @@ public class AStarSolver {
           else {
             heuris = Util.h2(boardPosition);
           }
-          PuzzleSolver unexploredNode = new PuzzleSolver(boardPosition, 
+          Node unexploredNode = new Node(boardPosition, 
                                                          heuris, 
                                                          path.toString());
           pQueue.add(unexploredNode);
@@ -64,6 +66,7 @@ public class AStarSolver {
     }
   }
  
+  // returns true if the inputted int[][] is the goal state
   public static boolean isGoalState(int[][] puzzle) {
     int count = 0;
     for (int i = 0; i < 3; i++) {
@@ -78,24 +81,15 @@ public class AStarSolver {
     return true;
   }
  
-  public static String hashKey(int[][] board) {
-    String key = "";
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 3; j++) {
-        key = key + Integer.toString(board[i][j]);
-      }
-    }
-    return key;
-  }
-  
+  // returns true if the board in question has already been part of a node that has been expanded
   public static boolean alreadyExpanded(int[][] board) {
     return set.contains(board);
   }
 }
 
-
-class BoardComparator implements Comparator<PuzzleSolver> {
-  public int compare(PuzzleSolver n1, PuzzleSolver n2) {
+// I had to override the compare method for the priority queue so I could put Nodes into queue
+class BoardComparator implements Comparator<Node> {
+  public int compare(Node n1, Node n2) {
     if (n1.heuristic > n2.heuristic) {
       return 1;
     }
